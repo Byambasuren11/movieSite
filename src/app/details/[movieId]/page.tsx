7cc396eac2a3ce5fa26b1ce683bf767162d36d15
Byambasuren11 <165979583+Byambasuren11@users.noreply.github.com>
2025-02-01 21:00:02 +0800

    a

--- src/app/details/[movieId]/page.tsx
+++ src/app/details/[movieId]/page.tsx
@@ -17,6 +17,7 @@ export default function Home() {
   const { movieId } = useParams();
   // console.log(id);
   const [clickedMovie, setClickedMovie] = useState<Movie>({} as Movie);
+  const [movie, setMovie] = useState<Movie>({} as Movie);
   const getClickedMovie = async () => {
     try {
       const response = await fetch(
@@ -29,9 +30,22 @@ export default function Home() {
       console.log(error);
     }
   };
+  const getDirector = async () => {
+    try {
+      const response = await fetch(
+        `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US&api_key=59e727c8b34f9b1acd7cf78c59abfe03`
+      );
+      const result = await response.json();
+      console.log(result);
+      setMovie(result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   console.log("dsdsad", clickedMovie);
   useEffect(() => {
     getClickedMovie();
+    getDirector();
   }, []);
   console.log("dcd", clickedMovie);
   return (
@@ -54,18 +68,23 @@ export default function Home() {
           <div className="w-[280px] h-[428px] bg-black rounded-sm overflow-hidden">
             <img
               className="h-[428px] object-cover"
+              src={`https://image.tmdb.org/t/p/original${clickedMovie.poster_path}`}
+            />
+          </div>
+          <div className="w-[760px] h-[428px] bg-black">
+            {" "}
+            <img
+              className="h-[428px] object-cover"
               src={`https://image.tmdb.org/t/p/original${clickedMovie.backdrop_path}`}
             />
           </div>
-          <div className="w-[760px] h-[428px] bg-black">video</div>
         </div>
         <div className="flex gap-2">
-
-        {clickedMovie.genres?.map((element,index) => ( 
-          <div className="inline-flex items-center border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground rounded-full text-xs w-fit">
-            {element.name}
-          </div>
-        ))}
+          {clickedMovie.genres?.map((element, index) => (
+            <div className="inline-flex items-center border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground rounded-full text-xs w-fit">
+              {element.name}
+            </div>
+          ))}
         </div>
         <div className="text-base">{clickedMovie.overview}</div>
         <div className="flex flex-col space-y-5 text-foreground mb-8">
