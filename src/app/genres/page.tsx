24b094dcf7f25111ef4e35e8b59249e6337a7517
Byambasuren11 <byumbasurenbold@gmail.com>
2025-02-07 17:13:23 +0800

    hoho

--- src/app/genres/page.tsx
+++ src/app/genres/page.tsx
@@ -5,13 +5,16 @@ import Star from "@/components/Star";
 import { ChevronRight, SearchCode } from "lucide-react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { useState, useEffect } from "react";
+import page from "../details/page";
+import Link from "next/link";
 
 const Genres = () => {
   const [clickedGenre, setClickedGenre] = useState();
-  const [genreMovie, setGenreMovie]=useState();
-  const router=useRouter();
-  const searchParams=useSearchParams();
-  const ids=(searchParams.get("genres") || "")?.split(",")
+  const [genreMovie, setGenreMovie] = useState();
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const ids = searchParams.get("genres") ? searchParams.get("genres")?.split(',') : "[]"
+  const page = (searchParams.get("page") || "1");
   const getClickedGenres = async () => {
     try {
       const response = await fetch(
@@ -29,29 +32,39 @@ const Genres = () => {
         `https://api.themoviedb.org/3/discover/movie?language=en&with_genres=${ids}&page=${page}&api_key=59e727c8b34f9b1acd7cf78c59abfe03`
       );
       const result = await response.json();
-      setGenreMovie(result.genres);
+      setGenreMovie(result.results);
     } catch (error) {
       console.log(error);
     }
   };
   const onClick1 = (id) => {
     const params = new URLSearchParams(searchParams.toString());
-   ids.push(id);
-    params.set("genres",ids?.join(","));
+    ids.push(id);
+    if(ids?.includes(id)){
+      console.log("hd",ids)
+    }
+    params.set("genres", ids?.join(","));
     console.log(params.toString());
     router.push(`?${params.toString()}`);
   };
-  console.log(ids)
+  
+  const onClick = () => {
+    const params = new URLSearchParams(searchParams.toString());
+    params.set("page", (Number(page) + 1).toString());
+    router.push(`?${params.toString()}`);
+    console.log(params.toString());
+  };
+  console.log("clic", genreMovie);
   useEffect(() => {
     getClickedGenres();
     getGenres();
-  }, []);
+  }, [searchParams]);
   return (
     <>
       <div className=" flex items-center flex-col">
         <Header />
-        <div>
-          <div className="flex w-[300px] gap-3 p-4 md:w-[300px] md:grid-cols-2 lg:w-[400px] flex-wrap cursor-pointer justify-start">
+        <div className="flex justify-between w-[1300px] mt-20">
+          <div className="flex w-[300px] gap-3 p-4 md:w-[300px] md:grid-cols-2 lg:w-[400px] flex-wrap cursor-pointer justify-start h-fit">
             {clickedGenre?.map((element, index) => (
               <div
                 className="inline-flex items-center border px-2.5 py-0.5 font-semibold transition-colors focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 text-foreground rounded-full text-xs w-fit"
@@ -63,9 +76,30 @@ const Genres = () => {
               </div>
             ))}
           </div>
-        </div>
-        <div>
-        
+          <div className="flex gap-6 flex-wrap w-[800px]">
+            {genreMovie?.map((element, index) => (
+              <Link
+                href={`/details/${element.id}`}
+                key={index}
+                className=" w-[180.5px] rounded-lg overflow-hidden cursor-pointer"
+              >
+                <img
+                  className=" h-[230px] w-full object-cover"
+                  src={`https://image.tmdb.org/t/p/original${element.poster_path}`}
+                />
+                <div className="h-[96px] bg-gray-200 p-2 text-extrabold">
+                  <p className="flex text-foreground text-sm items-center gap-x-1">
+                    <Star /> {element.vote_average.toFixed(1)}
+                    <span className="text-muted-foreground text-xs">/10</span>
+                  </p>
+                  <p className="h-14 overflow-hidden text-ellipsis line-clamp-2 text-lg text-foreground">
+                    {element.title}
+                  </p>
+                </div>
+              </Link>
+            ))}
+            <Paginations onClick={onClick} page={page} />
+          </div>
         </div>
       </div>
     </>
