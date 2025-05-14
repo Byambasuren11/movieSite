a23855961724a9b73dcc0b45e34db4aa71b1995a
Byambasuren11 <byumbasurenbold@gmail.com>
2025-05-14 15:38:00 +0800

    hihu

--- src/components/Up-Coming.tsx
+++ src/components/Up-Coming.tsx
@@ -45,7 +45,7 @@ export const Coming = () => {
               href={`/details/${element.id}`}
               // ID={id}
               key={index}
-              className=" group w-[457.5px] overflow-hidden rounded-lg lg:w-[230px]  "
+              className=" group w-[457.5px] overflow-hidden rounded-lg lg:w-[230px]  xl:w-[250px]"
             >
               <img
                 className=" h-[300px] w-full object-cover"
